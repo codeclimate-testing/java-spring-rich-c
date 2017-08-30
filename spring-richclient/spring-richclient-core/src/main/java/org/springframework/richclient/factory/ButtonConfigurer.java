c972c6783da93bf520905d29efe1874dd9a77c7d
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2017-08-30 17:02:45 -0400

    Initial commit

---
+++ spring-richclient/spring-richclient-core/src/main/java/org/springframework/richclient/factory/ButtonConfigurer.java
@@ -0,0 +1,26 @@
+/*
+ * $Header$
+ * $Revision: 1998 $
+ * $Date: 2008-03-06 07:18:03 -0500 (Thu, 06 Mar 2008) $
+ *
+ * Copyright Computer Science Innovations (CSI), 2004. All rights reserved.
+ */
+package org.springframework.richclient.factory;
+
+import javax.swing.AbstractButton;
+
+/**
+ * Marker for button configurers.
+ *
+ * @author Keith Donald
+ */
+public interface ButtonConfigurer {
+
+	/**
+	 * Configure the given button.
+	 *
+	 * @param button The button that needs to be configured.
+	 * @return the configured button.
+	 */
+    public AbstractButton configure(AbstractButton button);
+}
